6e657e7375cac4c81fd8a2bcd770960a2a893939
PrathamTelang <prathamtelang007@gmail.com>
2026-08-31 20:51:02 +0530

    added components/SearchBar.tsx

---
+++ components/SearchBar.tsx
@@ -0,0 +1,17 @@
+export default function SearchBar() {
+  return (
+    <input
+      placeholder="Search"
+      className="
+      w-full
+      border-b
+      border-zinc-800
+      bg-transparent
+      py-4
+      text-sm
+      outline-none
+      placeholder:text-zinc-600
+      "
+    />
+  )
+}
